e5190daa4fe69fb4aede3c41153fe2ac19ff8d6e
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2022-04-19 10:18:42 +0330

    Finished Part 2

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ const name=prompt("Type Your Name");
 const age=prompt("Type Your Age");
 const sex=prompt("Type Your Sex");
 
-alert(name + ' ' + age + ' '+ sex);
+alert(name + ' , ' + age + ' , '+ sex);
+
+const num1=prompt("Type Number1");
+const num2=prompt("Type Number2");
+alert(num1 + ' , ' + num2 );
+alert(num2 + ' , '+ num1);
