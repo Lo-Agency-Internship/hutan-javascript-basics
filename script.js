0f2e56796c4778ba40bc03391cbc9c7293a176b2
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2022-04-29 15:07:23 +0330

    Factorial Iterative and Recursive

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+// ========================== Intrative =====================
+  function factorialIterative(number) {
+
+    let get_number = number;
+    if(number === 0 || number === 1)
+    {
+        return 1;
+    }
+    else {
+        
+    while(number > 1)
+    {
+        number--;
+        get_number *= number;
+    }
+    return get_number;
+    }
+    
+  }
+
+  console.log("I Defined 6 for number Parameter (Iterative)");
+  console.log(factorialIterative(6)) ;
+
+// ========================== Recursive =====================
+
+  function factorialRecursive(number)
+{
+    if(number === 0 || number === 1)
+    {
+        return 1;
+    }
+    else{
+       return number * factorialRecursive(number -1)
+    }
+ 
+}
+console.log("I Defined 6 for number Parameter (Recursive)");
+console.log(factorialRecursive(6));
