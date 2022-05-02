c0ee68500c4d148c5674102a8b3172b5c30423a5
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2022-05-02 22:13:44 +0330

    Update customReduce

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 "use strict";
 
-// // // ========================== Intrative =====================
-// //   function factorialIterative(number) {
+ // ========================== Intrative =====================
+   function factorialIterative(number) {
 
-// //     let get_number = number;
-// //     if(number === 0 || number === 1)
-// //     {
-// //         return 1;
-// //     }
-// //     else {
+     let get_number = number;
+     if(number === 0 || number === 1)
+     {
+         return 1;
+     }
+     else {
         
-// //     while(number > 1)
-// //     {
-// //         number--;
-// //         get_number *= number;
-// //     }
-// //     return get_number;
-// //     }
+     while(number > 1)
+     {
+         number--;
+         get_number *= number;
+     }
+     return get_number;
+     }
     
-// //   }
+   }
 
-// //   console.log("I Defined 6 for number Parameter (Iterative)");
-// //   console.log(factorialIterative(6)) ;
+   console.log("I Defined 6 for number Parameter (Iterative)");
+   console.log(factorialIterative(6)) ;
 
-// // // ========================== Recursive =====================
+ // ========================== Recursive =====================
 
-// //   function factorialRecursive(number)
-// // {
-// //     if(number === 0 || number === 1)
-// //     {
-// //         return 1;
-// //     }
-// //     else{
-// //        return number * factorialRecursive(number -1)
-// //     }
+   function factorialRecursive(number)
+ {
+     if(number === 0 || number === 1)
+     {
+         return 1;
+     }
+     else{
+        return number * factorialRecursive(number -1)
+     }
  
-// // }
-// // console.log("I Defined 6 for number Parameter (Recursive)");
-// // console.log(factorialRecursive(6));
+ }
+ console.log("I Defined 6 for number Parameter (Recursive)");
+ console.log(factorialRecursive(6));
 
 
 
